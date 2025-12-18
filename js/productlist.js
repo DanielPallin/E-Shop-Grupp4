2092a1bac8ad34b2e2ba14ea737d2a8c59c78a76
Oscar Nilsson <oscar@ondproduktion.se>
2025-12-18 21:30:13 +0100

    Commented Js

--- js/productlist.js
+++ js/productlist.js
@@ -1,5 +1,29 @@
 (() => {
-    const buyBtns = document.querySelectorAll('button.buy'); // Declare array with all buyBtns on page.
+
+    /* Run buy buttons */
+    buyButtons();
+
+    /* Checking for changes to input field for mockup products. */
+    const input = document.querySelector('input#mockupProducts');
+    if (!input) return;
+
+    const minVal = input.getAttribute('min');
+    const maxVal = input.getAttribute('max');
+    const events = ['change', 'keyup'];
+
+    events.forEach(ev => {
+        input.addEventListener(ev, () => { // Check if events occur on input field.
+            let mockupProducts = Math.min(Math.max(input.value, minVal), maxVal); // Assign input value in interval between min and max.
+            input.value = mockupProducts; // Set input value to value in interval.
+            updateMockups(mockupProducts); // Update amount of mockup products.
+        });
+    });
+
+})();
+
+/* Making buyButtons add items to cart. */
+function buyButtons() {
+    let buyBtns = document.querySelectorAll('button.buy'); // Declare array with all buyBtns on page.
     const cartItems = document.querySelector('span.cartItems'); // Declare constant cartItems.
 
     if (buyBtns.length == 0 || !cartItems) return; // If there aren't any buyBtns or cartItems on page, stop.
@@ -13,48 +37,28 @@
             cartItems.innerHTML = i; // Replace .cartItems' current html with incremented value.
         });
     });
-})();
+}
 
-(() => {
+/* Creating mockup products */
+function updateMockups (mockupProducts) {
+    const main = document.querySelector('main'); // Declare main.
+    const productCards = main?.querySelectorAll('article.productCard'); // If main exists, create an array of all productCards in main.
 
-    const input = document.querySelector('input#mockupProducts');
-    const minVal = input.getAttribute('min');
-    const maxVal = input.getAttribute('max');
-    if (!input) return;
+    if (!productCards || productCards.length === 0) return; // If no productCards exist, stop.
 
-    const events = ['change', 'keyup'];
-
-    events.forEach(ev => {
-        input.addEventListener(ev, () => {
-            let mockupProducts = Math.min(Math.max(input.value, minVal), maxVal);
-            input.value = mockupProducts;
-            updateMockups(mockupProducts);
-        });
-    });
-
-
-    function updateMockups (mockupProducts) {
-        const main = document.querySelector('main');
-        const productCards = main?.querySelectorAll('article.productCard');
-
-        if (!productCards || productCards.length === 0) return;
-
-        if (productCards.length < mockupProducts) {
-            for (let index = productCards.length; index < mockupProducts; index++) {
-                article = document.createElement('article');
-                article.className = 'productCard';
-                article.innerHTML = productCards[0].innerHTML.split('1').join(index + 1);
-                main.append(article);
-            }
+    if (productCards.length < mockupProducts) { // If input value is greater than current amount of productCards.
+        for (let index = productCards.length; index < mockupProducts; index++) {
+            article = document.createElement('article'); // Create new productCard.
+            article.className = 'productCard';
+            article.innerHTML = productCards[0].innerHTML.split('1').join(index + 1);
+            main.append(article); // Append new productCard to main.
         }
-
-        if (productCards.length > mockupProducts) {
-            console.log('pC.length', productCards.length);
-            console.log('mockupProducts', mockupProducts);
-            for (let index = productCards.length; index > mockupProducts; index--) {
-                productCards[index-1].remove();
-            }
-        }
+        buyButtons(); // Refresh buyButtons so that new productCards get functional buttons.
     }
 
-})();
+    if (productCards.length > mockupProducts) { // If input value is less than current amount of productCards.
+        for (let index = productCards.length; index > mockupProducts; index--) {
+            productCards[index-1].remove(); // Remove productCard.
+        }
+    }
+}
