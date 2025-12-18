7747d1aa7597097dcd2edef41814ccd9ac38ab39
Oscar Nilsson <oscar@ondproduktion.se>
2025-12-18 21:08:46 +0100

    Added Js to create mockup products

--- js/productlist.js
+++ js/productlist.js
@@ -14,3 +14,47 @@
         });
     });
 })();
+
+(() => {
+
+    const input = document.querySelector('input#mockupProducts');
+    const minVal = input.getAttribute('min');
+    const maxVal = input.getAttribute('max');
+    if (!input) return;
+
+    const events = ['change', 'keyup'];
+
+    events.forEach(ev => {
+        input.addEventListener(ev, () => {
+            let mockupProducts = Math.min(Math.max(input.value, minVal), maxVal);
+            input.value = mockupProducts;
+            updateMockups(mockupProducts);
+        });
+    });
+
+
+    function updateMockups (mockupProducts) {
+        const main = document.querySelector('main');
+        const productCards = main?.querySelectorAll('article.productCard');
+
+        if (!productCards || productCards.length === 0) return;
+
+        if (productCards.length < mockupProducts) {
+            for (let index = productCards.length; index < mockupProducts; index++) {
+                article = document.createElement('article');
+                article.className = 'productCard';
+                article.innerHTML = productCards[0].innerHTML.split('1').join(index + 1);
+                main.append(article);
+            }
+        }
+
+        if (productCards.length > mockupProducts) {
+            console.log('pC.length', productCards.length);
+            console.log('mockupProducts', mockupProducts);
+            for (let index = productCards.length; index > mockupProducts; index--) {
+                productCards[index-1].remove();
+            }
+        }
+    }
+
+})();
